60da67e9aaa0414792fba1e5b61d096150a34cec
Nyaarium <project@nyaarium.com>
2023-06-06 13:43:45 -0700

    updated shape key remove

--- nyaatools.py
+++ nyaatools.py
@@ -3,9 +3,9 @@ from bpy.types import Operator, Panel
 from bpy.props import *
 from math import radians
 from mathutils import Matrix, Vector
-import numpy as np
 from collections import defaultdict
 from typing import Optional
+import numpy
 import webbrowser
 import traceback
 import time
@@ -37,6 +37,7 @@ PROP_AVATAR = "avatar"
 PROP_TARGET_AVATARS = "targetAvatars"
 PROP_EXPORT_PATH = "exportPath"
 A_POSE_SHOULDER_ANGLE = 15
+SHAPE_KEY_TOLERANCE = 0.001
 
 # Prop Key Constants
 #############################################
@@ -489,40 +490,62 @@ def removeUnusedVertexGroups(obj):
 
 
 def removeUnusedShapeKeys(obj):
-    if (obj.type != "MESH"):
-        return
-    if (not obj.data.shape_keys):
-        return
-    if (not obj.data.shape_keys.use_relative):
+    def debug_print(*msgs):
+        print("   ", *msgs)
         return
 
-    tolerance = 0.001
+    if obj.type == None:
+        raise BaseException("Expected a mesh object, got: None")
+    if obj.type != "MESH":
+        raise BaseException("Expected a mesh object, got: " + obj.type)
+    if not obj.data.shape_keys:
+        debug_print("No shape keys. Skipping", obj.name)
+        return
+    if not obj.data.shape_keys.use_relative:
+        debug_print("Shape keys not using relative mode. Skipping", obj.name)
+        return
 
+    # Get all the key blocks (shape keys) for the mesh
     kbs = obj.data.shape_keys.key_blocks
     nverts = len(obj.data.vertices)
     to_delete = []
-
-    # Cache locs for rel keys since many keys have the same rel key
     cache = {}
 
-    locs = np.empty(3*nverts, dtype=np.float32)
+    # Create an array to store the vertex locations
+    locs = numpy.empty(3 * nverts, dtype=numpy.float32)
 
+    # Loop through all the shape keys for the mesh
     for kb in kbs:
+        # Skip self
         if kb == kb.relative_key:
             continue
 
+        # Get the vertex locations for this shape key
         kb.data.foreach_get("co", locs)
 
+        # If we haven't already cached the relative key for this shape key,
+        # get the vertex locations for the relative key and cache them
         if kb.relative_key.name not in cache:
-            rel_locs = np.empty(3*nverts, dtype=np.float32)
+            rel_locs = numpy.empty(3 * nverts, dtype=numpy.float32)
             kb.relative_key.data.foreach_get("co", rel_locs)
             cache[kb.relative_key.name] = rel_locs
+
+        # Get the cached relative key vertex locations
         rel_locs = cache[kb.relative_key.name]
 
-        locs -= rel_locs
-        if (np.abs(locs) < tolerance).all():
+        # Calculate the largest distance between the relative key and the shape key
+        distances = numpy.abs(locs - rel_locs)
+        largestDistance = numpy.amax(distances)
+
+        # Print the largest distance
+        debug_print("Largest distance for", kb.name, "is", largestDistance)
+
+        # Check if all the vertex locations are within the tolerance
+        if (distances < SHAPE_KEY_TOLERANCE).all():
+            # If so, add this shape key to the list to delete
             to_delete.append(kb.name)
 
+    # Loop through all the shape keys to delete and remove them from the mesh
     for kb_name in to_delete:
         obj.shape_key_remove(obj.data.shape_keys.key_blocks[kb_name])
 
