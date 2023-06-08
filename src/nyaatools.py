a1a2bc5ada0d098a9774ffab63b7e8907da994c8
Nyaarium <project@nyaarium.com>
2023-06-07 19:11:03 -0700

    moved file to src

--- src/nyaatools.py
+++ src/nyaatools.py
@@ -2448,11 +2448,6 @@ if __name__ == "__main__":
 # bl_info = {
 #     "name":         "Apply modifier for object with shape keys",
 #     "author":       "Przemysław Bągard, additonal contributions by Iszotic, updated to 2.93 by Fro Zen",
-#     "blender":      (2,93,0),
-#     "version":      (0,2,1),
-#     "location":     "Context menu",
-#     "description":  "Apply modifier and remove from the stack for object with shape keys (Pushing 'Apply' button in 'Object modifiers' tab result in an error 'Modifier cannot be applied to a mesh with shape keys').",
-#     "category":     "Object Tools > Multi Shape Keys"
 # }
 
 def applyModifierForObjectWithShapeKeys(context, selectedModifiers, disable_armatures, callback_progress_tick=None):
