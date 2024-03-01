2a1410d86365346bf6fffbfb2f4860258a2c8e04
Nyaarium <project@nyaarium.com>
2024-03-01 15:38:03 -0800

    added helper find_scene_avatars

---
+++ NyaaTools/avatar/find_scene_avatars.py
@@ -0,0 +1,29 @@
+import bpy
+
+from ..common.get_prop import get_prop
+from ..consts import PROP_AVATAR_LAYERS
+from .get_avatar_layers import get_avatar_layers
+
+
+def find_scene_avatars(filter_to_avatar_name = None):
+    result = {}
+
+    for obj in bpy.data.objects:
+        obj_layers = get_avatar_layers(obj)
+
+        for avatar_name, layer_name in obj_layers:
+            if avatar_name not in result:
+                result[avatar_name] = {}
+
+            if layer_name not in result[avatar_name]:
+                result[avatar_name][layer_name] = {}
+
+            result[avatar_name][layer_name][obj.name] = obj
+
+    if filter_to_avatar_name:
+        if filter_to_avatar_name in result:
+            return result[filter_to_avatar_name]
+        else:
+            return None
+    else:
+        return result
