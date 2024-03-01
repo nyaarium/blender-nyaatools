0a968ce4074844c66ba1f1be7cb90c8ab5c57760
Nyaarium <project@nyaarium.com>
2024-03-01 15:57:10 -0800

    fixed renamer_restore

--- NyaaTools/common/renamer_restore.py
+++ NyaaTools/common/renamer_restore.py
@@ -2,11 +2,19 @@ import bpy
 
 
 def renamer_restore(unrename_info):
+    def get_obj_data(t, name):
+        if t == "mesh":
+            return bpy.data.meshes[name]
+        elif t == "armature":
+            return bpy.data.armatures[name]
+        else:
+            return None
+    
     for info in unrename_info:
         which = info[0]
         if which == "object":
             obj = bpy.data.objects[info[1]]
             obj.name = info[2]
         else:
-            data = bpy.data[info[0]][info[1]]
+            data = get_obj_data(which, info[1])
             data.name = info[2]
